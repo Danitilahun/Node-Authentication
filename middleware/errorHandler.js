702c097c2445ebe0d62c60f2cb557e95f1e449f0
Daniel Tilahun <tiledan2015@gmail.com>
2023-10-19 20:30:06 +0300

    Subject : adding essential middlewares Body : Middleware is added .middleware for credentials , error handleing , JWT  and for loginLimiter is added.

---
+++ middleware/errorHandler.js
@@ -0,0 +1,8 @@
+const errorHandler = (err, req, res, next) => {
+  console.log(err.stack);
+  const status = res.statusCode ? res.statusCode : 500; // server error
+  res.status(status);
+  res.json({ message: err.message, isError: true });
+};
+
+module.exports = errorHandler;
